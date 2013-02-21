a89536bff1ff3b51c1a8a37efeaa520021693bb5
Tyler Janzen <tylor.janzen@gmail.com>
2013-02-20 23:55:33 -0700

    New syntax for specifying ramdisk
    Updated syntax for specifying ramdisk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -40,7 +40,7 @@ TARGET_OTA_ASSERT_DEVICE := SGH-I757M,SGH-I757,celoxhd,SGHI757M,SGHI757
 BOARD_KERNEL_CMDLINE        := androidboot.hardware=qcom msm_watchdog.appsbark=0 msm_watchdog.enable=1 loglevel=4
 BOARD_KERNEL_BASE           := 0x48000000
 BOARD_KERNEL_PAGESIZE       := 2048
-BOARD_FORCE_RAMDISK_ADDRESS := 0x49500000
+BOARD_MKBOOTIMG_ARGS        := --ramdisk_offset 0x01400000
 TARGET_KERNEL_CONFIG        := cyanogenmod_celoxhd_defconfig
 TARGET_KERNEL_SOURCE        := kernel/samsung/msm8660-common
 
