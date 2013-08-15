76338d4bf44aeb0179f6ab687e1a29eebcc1d931
Tyler Janzen <tylor.janzen@gmail.com>
2013-08-15 10:34:57 -0600

    Update BoardConfig.mk
    Include BOARD_KERNEL_BASE as ours differs from the skyrocket.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -38,7 +38,7 @@ TARGET_OTA_ASSERT_DEVICE := SGH-I757M,SGH-I757,celoxhd,SGHI757M,SGHI757
 
 # Kernel
 #BOARD_KERNEL_CMDLINE        := androidboot.hardware=qcom msm_watchdog.appsbark=0 msm_watchdog.enable=1 loglevel=4
-#BOARD_KERNEL_BASE           := 0x48000000
+BOARD_KERNEL_BASE           := 0x48000000
 #BOARD_KERNEL_PAGESIZE       := 2048
 #BOARD_MKBOOTIMG_ARGS        := --ramdisk_offset 0x01400000
 TARGET_KERNEL_CONFIG        := cyanogenmod_celoxhd_defconfig
