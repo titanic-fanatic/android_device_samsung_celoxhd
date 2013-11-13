c77fbcc6b9f4bff1a86e40f00579f06ba7a6537c
Tyler Janzen <tylor.janzen@gmail.com>
2013-11-13 13:41:36 -0700

    Update build fingerprint

--- cm.mk
+++ cm.mk
@@ -9,7 +9,7 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGH-I757M TARGET_DEVICE=SGH-I757M BUILD_FINGERPRINT="samsung/SGH-I757M/SGH-I757M:4.4/IMM76D/UGMC5:user/release-keys" PRIVATE_BUILD_DESC="SGH-I757M-user 4.4 IMM76D UGMC5 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGH-I757M TARGET_DEVICE=SGH-I757M BUILD_FINGERPRINT="samsung/SGH-I757M/SGH-I757M:4.4/KRT16M/UGMC5:user/release-keys" PRIVATE_BUILD_DESC="SGH-I757M-user 4.4 KRT16M UGMC5 release-keys"
 
 PRODUCT_NAME := cm_celoxhd
 PRODUCT_DEVICE := celoxhd
