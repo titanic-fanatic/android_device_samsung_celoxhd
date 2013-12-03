61da6be2d60a03797ddc31c0187cc5c62bb3a97a
Tylor Janzen <tylor.janzen@gmail.com>
2013-12-03 03:31:47 -0700

    Moved vibrator.ko to /lib/modules instead of /tmp/system/lib/modules

--- device.mk
+++ device.mk
@@ -30,7 +30,7 @@ PRODUCT_COPY_FILES += \
     
 # Needed for vibrator to work in recovery
 PRODUCT_COPY_FILES += \
-	device/samsung/celoxhd/recovery/root/tmp/system/lib/modules/vibrator.ko:recovery/root/tmp/system/lib/modules/vibrator.ko \
+	device/samsung/celoxhd/recovery/root/lib/modules/vibrator.ko:recovery/root/lib/modules/vibrator.ko \
 	device/samsung/celoxhd/recovery/root/init.recovery.qcom.rc:recovery/root/init.recovery.qcom.rc
 
 # Inherit from celox-common
