0e389d5011182c4f9cfb467e021a2c09673f9f47
Tylor Janzen <tylor.janzen@gmail.com>
2013-12-08 00:22:21 -0700

    Update build fingerprint & add TARGET_SCREEN_HEIGHT/TARGET_SCREEN_WIDTH properties

--- cm.mk
+++ cm.mk
@@ -9,7 +9,10 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGH-I757M TARGET_DEVICE=SGH-I757M BUILD_FINGERPRINT="samsung/SGH-I757M/SGH-I757M:4.4/KRT16M/UGMC5:user/release-keys" PRIVATE_BUILD_DESC="SGH-I757M-user 4.4 KRT16M UGMC5 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGH-I757M TARGET_DEVICE=SGH-I757M BUILD_FINGERPRINT="samsung/SGH-I757M/SGH-I757M:4.4.1/KOT49E/UGMC5:user/release-keys" PRIVATE_BUILD_DESC="SGH-I757M-user 4.4.1 KOT49E UGMC5 release-keys"
+
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
 
 PRODUCT_NAME := cm_celoxhd
 PRODUCT_DEVICE := celoxhd
